b5283080a22b617038e1f8092b7dc0e5bd722e20
Nattanon Dungsunenarn <title409@icloud.com>
2016-01-12 12:35:48 +0700

    Add new structure

--- www/js/services.js
+++ www/js/services.js
@@ -8,31 +8,54 @@ angular.module('starter.services', [])
     excerpt: 'การแสดงความสามารถของหุ่นยนต์ และ การเขียนโปรแกรม',
     clublogo:'ion-social-apple',
     coverimg:'img/cover0.png'
-  }, {
+  },
+
+  {
     id: 1,
     name: 'เชียร์และแปรอักษร',
     excerpt: 'ของที่ระลึกและกิจกรรมร่วมสนุก',
     clublogo:'ion-ios-body',
+    clubid:'ก000',
     coverimg:'img/cover1.png'
-  }, {
+  },
+
+  {
     id: 2,
     name: 'ลูกเสือ',
     excerpt: 'ของที่ระลึกจากทีมลูกเสือสวนกุหลาบนนท์',
     clublogo:'ion-ios-flame',
+    clubid:'ก000',
     coverimg:'img/cover2.png'
-  }, {
+  },
+
+  {
     id: 3,
     name: 'อาร์ตสวนนนท์',
     excerpt: 'วาดภาพเหมือนและลุ้นของรางวัล',
     clublogo:'ion-ios-compose',
+    clubid:'ก000',
     coverimg:'img/cover3.png'
-  }, {
+  },
+
+   {
     id: 4,
-    name: 'กรรมการนักเรียน',
+    name: 'สภานักเรียน',
     excerpt: 'กิจกรรมการแข่งขันตอบคำถามเพื่อชิงรางวัล',
-    clublogo:'ion-ios-information',
+    clublogo:'ion-ios-people',
+    clubid:'ก000',
     coverimg:'img/cover4.png'
-  }];
+  },
+
+  {
+   id: 5,
+   name: 'การ์ตูน ทอล์ค',
+   excerpt: 'กิจกรรม และเกมส์ต่างๆ',
+   clublogo:'ion-ios-chatboxes',
+   clubid:'ก000',
+   coverimg:'img/cover4.png'
+ }
+
+];
 
   return {
     all: function() {
